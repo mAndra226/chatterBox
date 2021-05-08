d4efb2c3ef389bc744b8ba4029fe35895b1da5dc
mAndra226 <andrademarcoa20@gmail.com>
2021-05-07 22:49:12 -0700

    first commit

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,15 +1,43 @@
 import socket from './ws-client';
+import {UserStore} from './storage';
+import {ChatForm, ChatList, promptForUsername} from './dom';
+
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat="message-input"]';
+const LIST_SELECTOR = '[data-chat="message-list"]';
+
+// let username = '';
+let userStore = new UserStore('x-chattrbox/u');
+let username = userStore.get();
+
+if(!username) {
+    username = promptForUsername();
+    userStore.set(username);
+}
 
 class ChatApp {
     constructor() {
+        this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+        this.chatList = new ChatList(LIST_SELECTOR, username);
+
         socket.init('ws://localhost:3001');
         socket.registerOpenHandler(() => {
-            let message = new ChatMessage({ message: 'pow!' });
-
-            socket.sendMessage(message.serialize());
+            //let message = new ChatMessage({ message: 'pow!' });
+            //socket.sendMessage(message.serialize());
+            
+            this.chatForm.init((data) => {
+                let message = new ChatMessage(data);
+                
+                socket.sendMessage(message.serialize());
+            });
+            
+            this.chatList.init();
         });
         socket.registerMessageHandler((data) => {
             console.log(data);
+
+            let message = new ChatMessage(data);
+            this.chatList.drawMessage(message.serialize());
         });
     }
 }
@@ -17,7 +45,7 @@ class ChatApp {
 class ChatMessage {
     constructor({
         message: m,
-        user: u='batman',
+        user: u=username,
         timestamp: t=(new Date()).getTime()
     }) {
         this.user = u;
